45490d9de4fe2de6fc43b09f9fada2b2d44fed91
Nisha Khan <nisha.f.khan@gmail.com>
2020-07-29 21:44:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { artistName: "songTitle" }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [Slowdive]: "Alison" }, { [My Bloody Valentine]: "Sometimes"}, { [Phil Ochs]: "Here's to the State of Mississippi"})
+  return Object.assign({}, playlist, { [Slowdive]: Alison }, { ["Phil Ochs"]: "Here's to the State of Mississippi"}, { [My Bloody Valentine]: "Sometimes"})
 }
